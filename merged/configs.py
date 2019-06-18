33b1810c3924f36636a6a4872814b5ba9599d0f3
pat-hanbury <hanbury.p@husky.neu.edu>
2019-06-18 11:34:14 -0400

    updated configs

--- merged/configs.py
+++ merged/configs.py
@@ -1,11 +1,13 @@
 config = {
-    'name': 'ContinuedFrom0.1-lowerLR', # name of job
+    'name': 'HigherLRSFrom0.2', # name of job
     'num_particles' : 16,
     'num_steps' : 3000, # number of monte carlo steps
-    'deltas' : [x/20 for x in range(2,21)],
+    'deltas' : [x/20 for x in range(3,21)],
     'notes' : """
-              To figure out optimal LR and Iterations so that we can do 16 Particle enumeration
-              entirely
+              Continued from "HigherLRSFrom0.1" Tuesday @ 11:30am after it only
+              had done delta=0.1
+              
+              Purpose is to increase learning rates for the next few even more
               """,
     'visdom_port' : None,
     'visdom_server': None,
@@ -13,16 +15,15 @@ config = {
     'noise_scale' : 0.01,
     'Algorithm': 'Algorithm_1',
     'Enumeration' : True,
-    'checkpoint' : '/home/hanbury.p/feiguin/vmc/merged/outputs/16EnumContinued/2019-06-17-13:04/training_checkpoints/delta0.1_iter57.npy',
-    'learning_rates' : [ # 0.0 # 0.05
-                        0.05, # 0.10
-                        0.05, # 0.15
-                        0.1, # 0.20
-                        0.1, # 0.25
-                        0.05, # 0.30
-                        0.05, # 0.35
-                        0.05, # 0.40
-                        0.05, # 0.45
+    'checkpoint' : '/home/hanbury.p/feiguin/vmc/merged/outputs/HigherLRSFrom0.1/2019-06-18-09:25/training_checkpoints/delta0.1_iter27.npy',
+    'learning_rates' : [ # 0.0 # 0.05 # 0.10
+                        0.2, # 0.15
+                        0.2, # 0.20
+                        0.15, # 0.25
+                        0.15, # 0.30
+                        0.1, # 0.35
+                        0.1, # 0.40
+                        0.1, # 0.45
                         0.05, # 0.50
                         0.05, # 0.55
                         0.05, # 0.60
@@ -35,13 +36,12 @@ config = {
                         0.01, # 0.95
                         0.01, # 1.00 
                        ],# list of learning rates. If float, same LR will be used for all deltas
-    'iterations_list': [ # 0.0 # 0.05
-                        30, # 0.10
-                        75, # 0.15
-                        75, # 0.20
-                        75, # 0.25
-                        75, # 0.30
-                        75, # 0.35
+    'iterations_list': [ # 0.0 # 0.05 # 0.10
+                        90, # 0.15
+                        90, # 0.20
+                        90, # 0.25
+                        90, # 0.30
+                        90, # 0.35
                         90, # 0.40
                         90, # 0.45
                         90, # 0.50
